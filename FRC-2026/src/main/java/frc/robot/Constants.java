30f10650ddfa01886d9b680a072f56d820493918
scientistofeverything <noah.e.haskins@gmail.com>
2026-01-14 18:32:57 -0500

    changed everything to fit the kitbot code

--- FRC-2026/src/main/java/frc/robot/Constants.java
+++ FRC-2026/src/main/java/frc/robot/Constants.java
@@ -1,180 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.SensorDirectionValue;
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final class DriveConstants {
+    // Motor controller IDs for drivetrain motors
+    public static final int LEFT_LEADER_ID = 1;
+    public static final int LEFT_FOLLOWER_ID = 2;
+    public static final int RIGHT_LEADER_ID = 3;
+    public static final int RIGHT_FOLLOWER_ID = 4;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.Lib.Util.FalconSwerveConstants;
-import frc.robot.Lib.Util.SwerveModuleConstants;
+    // Current limit for drivetrain motors. 60A is a reasonable maximum to reduce
+    // likelihood of tripping breakers or damaging CIM motors
+    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 60;
+  }
 
-public class Constants {
+  public static final class FuelConstants {
+    // Motor controller IDs for Fuel Mechanism motors
+    public static final int FEEDER_MOTOR_ID = 6;
+    public static final int INTAKE_LAUNCHER_MOTOR_ID = 5;
 
-        public static final class Controllers {
-                public static final Joystick driver1 = new Joystick(0);
-                public static final XboxController driver2 = new XboxController(1);
-                public static final double stickDeadband = 0.3;
-        }
+    // Current limit and nominal voltage for fuel mechanism motors.
+    public static final int FEEDER_MOTOR_CURRENT_LIMIT = 60;
+    public static final int LAUNCHER_MOTOR_CURRENT_LIMIT = 60;
 
-        // Swerve Module Constants Class
-        public static final class Swerve {
+    // Voltage values for various fuel operations. These values may need to be tuned
+    // based on exact robot construction.
+    // See the Software Guide for tuning information
+    public static final double INTAKING_FEEDER_VOLTAGE = -12;
+    public static final double INTAKING_INTAKE_VOLTAGE = 10;
+    public static final double LAUNCHING_FEEDER_VOLTAGE = 9;
+    public static final double LAUNCHING_LAUNCHER_VOLTAGE = 10.6;
+    public static final double SPIN_UP_FEEDER_VOLTAGE = -6;
+    public static final double SPIN_UP_SECONDS = 1;
+  }
 
-                // Pigeon CAN ID
-                public static final int pigeonId = 13;
+  public static final class OperatorConstants {
+    // Port constants for driver and operator controllers. These should match the
+    // values in the Joystick tab of the Driver Station software
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
 
-                // Module Drive Ratios
-                public static final FalconSwerveConstants chosenModule = FalconSwerveConstants.SDS.MK4i
-                                .Falcon500(FalconSwerveConstants.SDS.MK4i.driveRatios.L69);
-
-                public static final FalconSwerveConstants module0 = FalconSwerveConstants.SDS.MK4i
-                                .Falcon500Inverted(FalconSwerveConstants.SDS.MK4i.driveRatios.L69);
-
-                // Drivetrain Constants
-                public static final double trackWidth = Units.inchesToMeters(29.0);
-                public static final double wheelBase = Units.inchesToMeters(29.0);
-                public static final double wheelCircumference = chosenModule.wheelCircumference;
-
-                public static final Translation2d flModuleOffset = new Translation2d(wheelBase / 2.0, trackWidth / 2.0);
-
-                // Swerve Kinematics
-                // Instead of each module being like a quadrant on a plane, the center being the
-                // origin, +x is front, +y is left, cause we did our modules counter-clockwise
-                public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
-                                new Translation2d(wheelBase / 2.0, -trackWidth / 2.0), // mod 0 front right
-                                // new Translation2d(wheelBase / 2.0, -trackWidth / 2.0), //mod1 front left
-                                new Translation2d(wheelBase / 2.0, trackWidth / 2.0), // mod1 front left
-                                // new Translation2d(-wheelBase / 2.0, trackWidth / 2.0), //mod2 back left
-                                new Translation2d(-wheelBase / 2.0, trackWidth / 2.0), // mod2 back left
-                                // new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0)); //mod3 back right
-                                new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0)); // mod3 back right
-
-                // Gear Ratios
-                public static final double driveGearRatio = chosenModule.driveGearRatio;
-                public static final double angleGearRatio = chosenModule.angleGearRatio;
-
-                // Motor Inverts
-                public static final InvertedValue angleMotorInvert = chosenModule.angleMotorInvert;
-                public static final InvertedValue driveMotorInvert = chosenModule.driveMotorInvert;
-
-                // Angle Encoder Invert
-                public static final SensorDirectionValue cancoderInvert = chosenModule.cancoderInvert;
-
-                // Swerve Current Limiting
-                public static final int angleCurrentLimit = 25;
-                public static final int angleCurrentThreshold = 40;
-                public static final double angleCurrentThresholdTime = 0.1;
-                public static final boolean angleEnableCurrentLimit = true;
-
-                public static final int driveCurrentLimit = 100;
-                public static final int driveCurrentThreshold = 120;
-                public static final double driveCurrentThresholdTime = 0.1;
-                public static final boolean driveEnableCurrentLimit = true;
-
-                /*
-                 * These values are used by the drive falcon to ramp in open loop and closed
-                 * loop driving.
-                 * We found a small open loop ramp (0.25) helps with tread wear, tipping, etc
-                 */
-                public static final double openLoopRamp = 0.25;
-                public static final double closedLoopRamp = 0.0;
-
-                // Angle Motor PID Values
-                public static final double angleKP = chosenModule.angleKP;
-                public static final double angleKI = chosenModule.angleKI;
-                public static final double angleKD = chosenModule.angleKD;
-
-                // Drive Motor PID Values
-                public static final double driveKP = 0.12;// 0.08;//2.0;
-                public static final double driveKI = 0.0;// 0.01;//40.0;
-                public static final double driveKD = 0.00;// 0.02;
-                public static final double driveKF = 0.0;
-
-                public static final double driveKS = 0.32;
-                public static final double driveKV = 1.51;
-                public static final double driveKA = 0.27;
-
-                // Swerve Profiling Values
-                // Meters per Second
-                public static final double maxSpeed = 5.21;
-                // Radians per Second
-                public static final double driveRadius = Math.hypot(wheelBase, trackWidth) / 2.0;
-                public static final double maxAngularVelocity = maxSpeed / driveRadius;
-
-                // Neutral Modes
-                public static final NeutralModeValue angleNeutralMode = NeutralModeValue.Coast;
-                public static final NeutralModeValue driveNeutralMode = NeutralModeValue.Brake;
-
-                // Module Specific Constants
-                /* Front Right Module - Module 0 */
-                public static final class Mod0 {
-                        public static final int driveMotorID = 37;
-                        public static final int angleMotorID = 27;
-                        public static final int canCoderID = 21;
-                        public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-25.66);// 0 original offset
-                        public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID,
-                                        angleMotorID,
-                                        canCoderID, angleOffset);
-                }
-
-                /* Front Left Module - Module 1 */
-                public static final class Mod1 {
-                        public static final int driveMotorID = 38;
-                        public static final int angleMotorID = 14;
-                        public static final int canCoderID = 12;
-                        public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-78.86);// -45 original
-                                                                                                    // offset
-                        public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID,
-                                        angleMotorID,
-                                        canCoderID, angleOffset);
-                }
-
-                /* Back Left Module - Module 2 */
-                public static final class Mod2 {
-                        public static final int driveMotorID = 39;
-                        public static final int angleMotorID = 18;
-                        public static final int canCoderID = 19;
-                        public static final Rotation2d angleOffset = Rotation2d.fromDegrees(12.26);// 9 original offset
-                        public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID,
-                                        angleMotorID,
-                                        canCoderID, angleOffset);
-                }
-
-                /* Back Right Module - Module 3 */
-                public static final class Mod3 {
-                        public static final int driveMotorID = 36;
-                        public static final int angleMotorID = 10;
-                        public static final int canCoderID = 25;
-                        public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-149.86);// 34.75 original
-                                                                                                     // offset
-                        public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID,
-                                        angleMotorID,
-                                        canCoderID, angleOffset);
-                }
-
-                // For Pathplanner Config - DO NOT USE ANYWHERE EXCEPT FOR THE CONFIG IN AUTO!!
-                public static final Translation2d front_offset = new Translation2d(wheelBase / 2.0, trackWidth / 2.0);
-
-                public static final class AutoConstants {
-                        public static final double kMaxSpeedMetersPerSecond = 3;
-                        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-                        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-                        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-
-                        public static final double kPXController = 1;
-                        public static final double kPYController = 1;
-                        public static final double kPThetaController = 1;
-
-                        // Constraint for the motion profilied robot angle controller
-                        public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
-                                        kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
-                }
-
-        }
+    // This value is multiplied by the joystick value when rotating the robot to
+    // help avoid turning too fast and beign difficult to control
+    public static final double DRIVE_SCALING = .7;
+    public static final double ROTATION_SCALING = .8;
+  }
 }
--- FRC-2026/src/main/java/frc/robot/Robot.java
+++ FRC-2026/src/main/java/frc/robot/Robot.java
@@ -4,41 +4,40 @@
 
 package frc.robot;
 
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Translation2d;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.Subsystems.Swerve;
-import frc.robot.RobotContainer;
 
 /**
- * The methods in this class are called automatically corresponding to each
- * mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the
- * package after creating
- * this project, you must also update the Main.java file in the project.
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-  public RobotContainer robotContainer;
-  private Command m_AutonomousCommand;
+  private Command m_autonomousCommand;
 
-  // Swerve Drive Varibles
-  public static final CTREConfigs ctreConfigs = new CTREConfigs();
-  public Swerve swerve;
+  private RobotContainer m_robotContainer;
 
   /**
    * This function is run when the robot is first started up and should be used
    * for any
    * initialization code.
    */
-  public Robot() {
-    swerve = Swerve.getInstance();
-    robotContainer = new RobotContainer();
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+
+    // Used to track usage of Kitbot code, please do not remove.
+    HAL.report(tResourceType.kResourceType_Framework, 10);
   }
 
   /**
@@ -53,78 +52,64 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    swerve.swerveCurrents();
-    RobotTelemetry();
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
   }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different
-   * autonomous modes using the dashboard. The sendable chooser code works with
-   * the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
-   * chooser code and
-   * uncomment the getString line to get the auto name from the text box below the
-   * Gyro
-   *
-   * <p>
-   * You can add additional auto modes by adding additional comparisons to the
-   * switch structure
-   * below with additional strings. If using the SendableChooser make sure to add
-   * them to the
-   * chooser code above as well.
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
    */
   @Override
   public void autonomousInit() {
-    Command m_AutonomousCommand = robotContainer.getAutonomousCommand();
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
-    if (m_AutonomousCommand != null) {
-      m_AutonomousCommand.schedule();
+    if (m_autonomousCommand != null) {
+     CommandScheduler.getInstance().schedule(m_autonomousCommand);
     }
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    swerve.swerveOdometry.update(swerve.getGyroYaw(), swerve.getModulePositions());
-    RobotTelemetry();
   }
 
-  /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {// Destroy Auto Commands When Switching To TeleOP
-    if (m_AutonomousCommand != null) {
-      m_AutonomousCommand.cancel();
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
-
-    swerve.zeroHeading();
-    RobotTelemetry();
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    swerve.swerveOdometry.update(swerve.getPosGyroYaw(), swerve.getModulePositions());
-
-    Driver1Controls();
-
-    RobotTelemetry();
   }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {
-  }
-
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /** This function is called once when test mode is enabled. */
   @Override
   public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
   }
 
   /** This function is called periodically during test mode. */
@@ -140,37 +125,5 @@ public class Robot extends TimedRobot {
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {
-  }
-
-  // Add Telemetry Data for Robot
-  private void RobotTelemetry() {
-
-  }
-
-  private void Driver1Controls() {
-    // Back to robot centric while button seven is pushed
-    if (Constants.Controllers.driver1.getRawButton(2)) {
-      swerve.zeroHeading();
-      System.out.println("Gyro reset");
-    }
-  }
-
-  private void SwerveDrive(boolean isFieldRel) {
-    // Controller Deadbands (Translation, Strafe, Rotation)
-
-    double xSpeed = MathUtil.applyDeadband(Constants.Controllers.driver1.getRawAxis(1)
-        * (Constants.Controllers.driver1.getRawAxis(2)),
-        Constants.Controllers.stickDeadband);
-    double ySpeed = MathUtil.applyDeadband(Constants.Controllers.driver1.getRawAxis(0)
-        * (Constants.Controllers.driver1.getRawAxis(2)),
-        Constants.Controllers.stickDeadband);
-    double rot = MathUtil.applyDeadband(Constants.Controllers.driver1.getRawAxis(3) // we made it unnegatived
-        * (Constants.Controllers.driver1.getRawAxis(2)),
-        Constants.Controllers.stickDeadband);
-
-    // Drive Function
-    swerve.drive(new Translation2d(xSpeed, ySpeed).times(Constants.Swerve.maxSpeed),
-        rot * Constants.Swerve.maxAngularVelocity, isFieldRel, false);
-
   }
 }
