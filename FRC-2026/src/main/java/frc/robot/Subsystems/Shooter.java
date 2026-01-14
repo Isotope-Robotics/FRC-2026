f4886b8b18d9ac4c783fffa495d529116fc4467e
ChillMarto <sammyrittmann150@gmail.com>
2026-01-14 18:36:39 -0500

    robot-development 1

--- FRC-2026/src/main/java/frc/robot/Subsystems/Shooter.java
+++ FRC-2026/src/main/java/frc/robot/Subsystems/Shooter.java
@@ -2,6 +2,32 @@ package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 public class Shooter extends SubsystemBase {
-    
+    public SparkMax shooterMotor;
+    public RelativeEncoder shooterEncoder;
+
+    private Shooter(int shooterMotorID){
+        shooterMotor = new SparkMax(shooterMotorID, MotorType.kBrushless);
+        shooterEncoder = shooterMotor.getEncoder();
+        
+        SparkMaxConfig shooterConfig = new SparkMaxConfig();
+        shooterConfig.idleMode(IdleMode.kBrake);
+        shooterMotor.configure(shooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    }
+
 }
