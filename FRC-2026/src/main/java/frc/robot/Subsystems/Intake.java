afdec97478b81ce1eeb04f9b4eb3d08747ee0a47
Shahanssp <gx7sjyc5mg@privaterelay.appleid.com>
2026-01-14 19:34:40 -0500

    Added motor initialization and configuration

--- FRC-2026/src/main/java/frc/robot/Subsystems/Intake.java
+++ FRC-2026/src/main/java/frc/robot/Subsystems/Intake.java
@@ -3,5 +3,12 @@ package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    
+    public SparkMax intakeMotor; 
+    public SparkMaxConfig intakeMotorConfig;
+
+    public Intake(int IntakeMotorID) {
+        intakeMotor = new SparkMax(IntakeMotorID, MotorType.kBrushless);
+        intakeMotorConfig = new SparkMaxConfig();
+        intakeMotor.configure(intakeMotorConfig);
+    }
 }
