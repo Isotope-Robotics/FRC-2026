d81cbe2da34b3c32de74173a34544530783d73dd
scientistofeverything <noah.e.haskins@gmail.com>
2026-01-15 17:52:00 -0500

    added singleton structure to subsystems (whoops)

--- FRC-2026/src/main/java/frc/robot/Subsystems/Intake.java
+++ FRC-2026/src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,14 +1,36 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake extends SubsystemBase {
-    public SparkMax intakeMotor; 
-    public SparkMaxConfig intakeMotorConfig;
+import frc.robot.Constants;
 
-    public Intake(int IntakeMotorID) {
-        intakeMotor = new SparkMax(IntakeMotorID, MotorType.kBrushless);
-        intakeMotorConfig = new SparkMaxConfig();
-        intakeMotor.configure(intakeMotorConfig);
+public class Intake extends SubsystemBase {
+
+    public SparkMax intakeMotor; 
+
+    private static Intake m_Instance;
+
+    private Intake (int intakeMotorID) {
+
+        intakeMotor = new SparkMax(intakeMotorID, MotorType.kBrushless);
+
+        SparkMaxConfig intakeConfig = new SparkMaxConfig();
+        intakeConfig.idleMode(Constants.Intake.intakeMotorIdleMode);
+        intakeConfig.inverted(Constants.Intake.intakeMotorInvert);
+        intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        
     }
+
+    public static Intake getInstance () {
+        if (m_Instance == null)
+            m_Instance = new Intake(Constants.Intake.intakeMotorID);
+        return m_Instance;
+    }
+
 }
--- FRC-2026/src/main/java/frc/robot/Subsystems/Shooter.java
+++ FRC-2026/src/main/java/frc/robot/Subsystems/Shooter.java
@@ -17,18 +17,28 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
+
     public SparkMax shooterMotor;
     public RelativeEncoder shooterEncoder;
 
+    private static Shooter m_Instance = null;
+
     private Shooter (int shooterMotorID) {
-        
+
         shooterMotor = new SparkMax(shooterMotorID, MotorType.kBrushless);
         shooterEncoder = shooterMotor.getEncoder();
         
         SparkMaxConfig shooterConfig = new SparkMaxConfig();
-        shooterConfig.idleMode(IdleMode.kBrake);
+        shooterConfig.idleMode(Constants.Shooter.shooterMotorIdleMode);
+        shooterConfig.inverted(Constants.Shooter.shooterMotorInvert);
         shooterMotor.configure(shooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
+    }
+
+    public static Shooter getInstance () {
+        if (m_Instance == null)
+            m_Instance = new Shooter(Constants.Shooter.shooterMotorID);
+        return m_Instance;
     }
 
 }
