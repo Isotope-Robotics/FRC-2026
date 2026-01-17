8faf904bee70190cac1caa21a8115fb783d24931
Shahanssp <gx7sjyc5mg@privaterelay.appleid.com>
2026-01-17 11:27:31 -0500

    Removed unnecessary overrides

--- FRC-2026/src/main/java/frc/robot/Subsystems/Intake.java
+++ FRC-2026/src/main/java/frc/robot/Subsystems/Intake.java
@@ -26,24 +26,20 @@ public class Intake extends SubsystemBase {
         intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         
     }
-    
-    @Override
+
     public void set(double value) {
         intakeMotor.set(value);
     }
 
-    @Override
     public void stop() {
         intakeMotor.set(0);
     }
 
     //May need to switch the signs on intake and outtake
-    @Override
     public void intake() {
         intakeMotor.set(-1);
     }
 
-    @Override
     public void outtake() {
         intakeMotor.set(1);
     }
