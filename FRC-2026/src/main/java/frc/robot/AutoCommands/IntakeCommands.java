f6fa7ab9582435d92d2a354252223cfbbd626555
scientistofeverything <noah.e.haskins@gmail.com>
2026-01-19 22:00:49 -0500

    added auto commands

---
+++ FRC-2026/src/main/java/frc/robot/AutoCommands/IntakeCommands.java
@@ -0,0 +1,23 @@
+package frc.robot.AutoCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Subsystems.Intake;
+
+public class IntakeCommands {
+
+    public static Intake intake = Intake.getInstance();
+
+    public static Command runIntake () {
+        return Commands.runOnce(() -> {
+            intake.intake();
+        });
+    }
+
+    public static Command stopIntake () {
+        return Commands.runOnce(() -> {
+            intake.stop();
+        });
+    }
+
+}
--- FRC-2026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026/src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.AutoCommands.IntakeCommands;
 
 //Class for Auto Commands Only
 public class RobotContainer {
@@ -14,16 +15,8 @@ public class RobotContainer {
     public RobotContainer(){
 
         // ADD AUTO COMMANDS HERE
-        // NamedCommands.registerCommand("DropIntake", dropToPickUp);
-        // NamedCommands.registerCommand("UpIntake", upIntake);
-        // NamedCommands.registerCommand("IntakeNote", IntakeCommands.IntakeNote());
-        // NamedCommands.registerCommand("Shoot", ShooterCommands.shoot());
-        // NamedCommands.registerCommand("IntakeStop", IntakeCommands.stopIntake());
-        // NamedCommands.registerCommand("IntakeLess", IntakeCommands.IntakeLess());
-        // NamedCommands.registerCommand("StopWrist", IntakeCommands.StopWrist());
-        // NamedCommands.registerCommand("NoteAutoAim", SwerveCommands.NoteAutoAim());
-        // NamedCommands.registerCommand("LessNoteAutoAim", SwerveCommands.LessNoteAutoAim());
-        // NamedCommands.registerCommand("Shoot2", ShooterCommands.shoot2());
+        NamedCommands.registerCommand("Run Intake", IntakeCommands.runIntake());
+        NamedCommands.registerCommand("Stop Intake", IntakeCommands.stopIntake());
 
         autoChooser = AutoBuilder.buildAutoChooser(); // Default auto will be `Commands.none()`
         SmartDashboard.putData("Auto Mode", autoChooser);
