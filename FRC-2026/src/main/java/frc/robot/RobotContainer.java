6c4c7cf0f9d195b33c8589c0431c8ef007f21fff
ChillMarto <sammyrittmann150@gmail.com>
2026-01-21 18:34:41 -0500

    pathplanner auto added autocommands

--- FRC-2026/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2026/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.AutoCommands.IntakeCommands;
+import frc.robot.AutoCommands.ShooterCommands;
 
 //Class for Auto Commands Only
 public class RobotContainer {
@@ -17,6 +18,8 @@ public class RobotContainer {
         // ADD AUTO COMMANDS HERE
         NamedCommands.registerCommand("Run Intake", IntakeCommands.runIntake());
         NamedCommands.registerCommand("Stop Intake", IntakeCommands.stopIntake());
+        NamedCommands.registerCommand("Run Shooter", ShooterCommands.runShooter());
+        NamedCommands.registerCommand("Stop Shooter", ShooterCommands.stopShooter());
 
         autoChooser = AutoBuilder.buildAutoChooser(); // Default auto will be `Commands.none()`
         SmartDashboard.putData("Auto Mode", autoChooser);
