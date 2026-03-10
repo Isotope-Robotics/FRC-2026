4fd58a0afba051ffdaf5ad103a512c1a92d93b98
Ethen <ebot14rocks@gmail.com>
2026-03-10 16:38:38 -0500

    Fixed Auto command scheduler to not use deprecated functions

--- FRC-2026/src/main/java/frc/robot/Robot.java
+++ FRC-2026/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Subsystems.*;
 import frc.robot.Subsystems.Shooter.ShooterState;
 import frc.robot.Subsystems.Intake.IntakeState;
@@ -90,7 +91,7 @@ public class Robot extends TimedRobot {
 
     // schedule the autonomous command (example)
     if (m_AutonomousCommand != null) {
-      m_AutonomousCommand.schedule();
+      CommandScheduler.getInstance().schedule(m_AutonomousCommand);
     }
   }
 
