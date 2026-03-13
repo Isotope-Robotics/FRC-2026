c8b497b5c9e508170f10c7d45ba330b1fb5cb52c
Devin-M5706 <dmyers5706@gmail.com>
2026-03-13 16:21:00 -0400

    shooter2MotorID differentiated

--- FRC-2026/src/main/java/frc/robot/subsystems/Shooter.java
+++ FRC-2026/src/main/java/frc/robot/subsystems/Shooter.java
@@ -46,7 +46,7 @@ public class Shooter extends SubsystemBase {
         shooterOutput.NeutralMode = Constants.Shooter.shooterMotorIdleMode;
         shooterMotor.getConfigurator().apply(shooterConfig);
 
-        shooter2Motor = new TalonFX(shooterMotorID);
+        shooter2Motor = new TalonFX(shooter2MotorID);
 
         TalonFXConfiguration shooter2Config = new TalonFXConfiguration();
         MotorOutputConfigs shooter2Output = shooterConfig.MotorOutput;
